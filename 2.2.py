71e5343b19b2b07c4e2340530e55144cc995391f
anmol14dev <img_2018012@iiitm.ac.in>
2021-01-30 15:53:24 +0530

    pointer

--- 2.2.py
+++ 2.2.py
@@ -1,2 +1,20 @@
 n, m, k = [int(x) for x in input().split()]
-desiredList = [(max(int(x) - k, 0), int(x) + k) for x in input().split()]
+desiredList = sorted([int(x) for x in input().split()])
+size = sorted([int(x) for x in input().split()])
+upperPointer, lowerPointer = 0, 0
+count = 0
+print(desiredList)
+print(size)
+while upperPointer < n and lowerPointer < m:
+    if (
+        max(desiredList[upperPointer] - k, 0)
+        <= size[lowerPointer]
+        <= desiredList[upperPointer] + k
+    ):
+        count += 1
+        lowerPointer += 1
+    elif size[lowerPointer] < max(desiredList[upperPointer] - k, 0):
+        lowerPointer += 1
+    else:
+        upperPointer += 1
+print(count)
